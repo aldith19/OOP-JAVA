b21fd6535f2a3efd251400ad9631a199710fe9be
aldith19 <aldith.reynaldi23@gmail.com>
2023-10-11 15:44:34 +0700

    OOP Java

--- src/LaptopApp.java
+++ src/LaptopApp.java
@@ -11,6 +11,19 @@ public class LaptopApp {
         Msi msi = new Msi("Cyborg",12500000, "Processor I5-12100F");
         msi.brosur("MSI ");
 
+        /* Perubahan bentuk dalam Polymorphisme */
+
+        Laptop laptop1 = new Laptop("Predator",17500000, "Processor AMD Ryzen 75000");
+        laptop1.brosur("Acer ");
+
+        laptop1 = new Lenovo("IdelPad",7500000, "Processor Intel I3-12100F");
+        laptop1.brosur("Lenovo ");
+
+        laptop1 = new Msi("Cyborg",12500000, "Processor I5-12100F");
+        laptop1.brosur("MSI ");
+
+
+
 
     }
 }
