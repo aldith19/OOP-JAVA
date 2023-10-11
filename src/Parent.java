4193ff5bdcd71aaecbd93bd3cf22e0c216b16c95
aldith19 <aldith.reynaldi23@gmail.com>
2023-10-11 21:58:31 +0700

    OOP Java

--- src/Parent.java
+++ src/Parent.java
@@ -2,18 +2,15 @@
     String name;
 
     void doit(){
-        System.out.println("Do it parent");
+        System.out.println("ini punya parent");
     }
 
 }
-
-
  class Child extends Parent {
      String name;
-
      void doit(){
-         System.out.println("Do it child");
-         System.out.println("Do it parent name " + super.name);
+         System.out.println("ini punya child");
+         System.out.println("parent " + super.name);
      }
 
  }
--- src/ParentApp.java
+++ src/ParentApp.java
@@ -1,12 +1,15 @@
 public class ParentApp {
     public static void main(String[] args) {
 
+
+
         Child child = new Child();
         child.name = "Eko";
         child.doit();
         System.out.println(child.name);
 
         Parent parent = (Parent) child;
+        parent.name ="budi";
         parent.doit();
         System.out.println(parent.name);
     }
