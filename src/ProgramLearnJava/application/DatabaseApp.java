e1a520d5ec1a41276b01b299c28ddc4ef41bc155
aldith19 <aldith.reynaldi23@gmail.com>
2023-10-14 21:38:11 +0700

    OOP Java

--- src/ProgramLearnJava/application/DatabaseApp.java
+++ src/ProgramLearnJava/application/DatabaseApp.java
@@ -16,7 +16,7 @@ public class DatabaseApp {
 
     public static void connectDatabase(String username, String password){
         if (username == null || password == null){
-            throw new DatabaseError("Tidak bisa koneksi ke databse");
+            throw new DatabaseError("Tidak bisa koneksi ke database");
         }
     }
 }
--- src/ProgramLearnJava/application/ValidationApp.java
+++ src/ProgramLearnJava/application/ValidationApp.java
@@ -9,7 +9,7 @@ public class ValidationApp {
 
         LoginRequest loginRequest = new LoginRequest("eko","1cak");
         /* blok Try catch untuk menangkap error, Bisa pake ini | buat error excepion secara banyak */
-        /* Final keyword blok untuk dimana selalu eksekusi  baik terjadi exception atau tidak,  mau itu sukses atau gagal */
+        /* Final keyword blok untuk dimana selalu eksekusi baik terjadi exception atau tidak, mau itu sukses atau gagal */
         try{
         ValidationUtil.validate(loginRequest);
             System.out.println("Data Valid");
@@ -30,6 +30,7 @@ public class ValidationApp {
             System.out.println("Data Valid");
         }catch (VallidationException  | NullPointerException exception){
             System.out.println("Data Tidak Valid: " + exception.getMessage());
+
         }catch (NullPointerException exception){
             System.out.println("Data Kosong: " + exception.getMessage() );
         }
