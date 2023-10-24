baa1d17d0cb6d5ec11c3c56f9021d2dcf01b919f
aldith19 <aldith.reynaldi23@gmail.com>
2023-10-24 16:50:45 +0700

    OOP Java

--- src/PayrollSystem/PayrollSystemApp.java
+++ src/PayrollSystem/PayrollSystemApp.java
@@ -6,7 +6,6 @@ public class PayrollSystemApp {
         FullTimeEmployee satu = new FullTimeEmployee("malik",1,3500000L);
         PartTime dua  = new PartTime("hendri",2,40,300);
 
-
         payrollSystem.addEmployee(satu);
         payrollSystem.addEmployee(dua);
         System.out.println("Detail Employee");
